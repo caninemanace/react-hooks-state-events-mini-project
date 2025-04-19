2f137b7597fb8a205449dc5c139928dd9bd8f824
Eddie Njoroge <eddie.njoroge2306@gmail.com>
2025-04-19 12:32:57 +0300

    assignment commpleted

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  // Delete Task Handler
+  function handleDelete(deletedTaskText) {
+    const updatedTasks = tasks.filter((task) => task.text !== deletedTaskText);
+    setTasks(updatedTasks);
+  }
+
+  // Category Filter Handler
+  function handleCategoryChange(category) {
+    setSelectedCategory(category);
+  }
+
+  // Add Task Handler
+  function handleAddTask(newTask) {
+    setTasks([...tasks, newTask]);
+  }
+
+  
+  const visibleTasks =
+    selectedCategory === "All"
+      ? tasks
+      : tasks.filter((task) => task.category === selectedCategory);
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedCategory={selectedCategory}
+        onCategoryChange={handleCategoryChange}
+      />
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={handleAddTask} />
+      <TaskList tasks={visibleTasks} onDeleteTask={handleDelete} />
     </div>
   );
 }
 
 export default App;
+
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,19 @@
 import React from "react";
 
-function Task() {
+function Task({ text, category, onDelete }) {
+  function handleClick() {
+    onDelete(text); // Send the task text back to App.js for deletion
+  }
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={handleClick}>X</button>
     </div>
   );
 }
 
 export default Task;
+
+
